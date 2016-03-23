2ba3849a175564a871cd623d8bc9dcf1fe2fa871
Rachel030219 <291054446@qq.com>
2016-03-24 06:58:29 +0800

    增加DrawerLayout

--- app/src/main/java/com/randegroup/roid/MainActivity.java
+++ app/src/main/java/com/randegroup/roid/MainActivity.java
@@ -1,13 +1,20 @@
 package com.randegroup.roid;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.content.Intent;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity{
 	Toolbar toolbar;
+	DrawerLayout mDrawerLayout;
+	ActionBarDrawerToggle mDrawerToggle;
+	ListView mDrawerItems;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -18,10 +25,28 @@ public class MainActivity extends AppCompatActivity{
 	// 绑定控件
 	public void bindViews(){
 		toolbar = (Toolbar)findViewById(R.id.main_toolbar);
+		mDrawerLayout = (DrawerLayout)findViewById(R.id.main_drawerlayout);
+		mDrawerItems = (ListView)findViewById(R.id.main_drawer_items);
 	}
-	// 设置Toolbar
+	// 设置Toolbar和抽屉
 	public void setupToolbar(){
 		toolbar.setTitle(getTitle());
 		setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+				// 打开DrawerLayout
+                super.onDrawerOpened(drawerView);
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+				// 关闭DrawerLayout
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 	}
 }
