0a239c53ae92752d2a9422597f67ca67a71ba0a8
Rachel030219 <291054446@qq.com>
2016-03-25 23:01:25 +0800

    更新了界面，优化了内存占用

--- app/src/main/java/com/randegroup/roid/MainActivity.java
+++ app/src/main/java/com/randegroup/roid/MainActivity.java
@@ -1,38 +1,57 @@
 package com.randegroup.roid;
 
+import java.io.File;
 import java.util.Map;
-import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
-import android.graphics.Bitmap;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.util.ArrayMap;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.support.v4.widget.DrawerLayout;
 import android.support.design.widget.NavigationView;
 
 public class MainActivity extends AppCompatActivity{
-	Toolbar toolbar;
-	DrawerLayout mDrawerLayout;
-	NavigationView mNavView;
-	ActionBarDrawerToggle mDrawerToggle;
+	// 控件
+	private Toolbar toolbar;
+	private DrawerLayout mDrawerLayout;
+	private NavigationView mNavView;
+	private ActionBarDrawerToggle mDrawerToggle;
+	// Id映射，通过String存取
+	private Map<String,Integer> mIdMap;
+	// 存入Fragment
+	private List<Fragment> mFragmentList;
+	// Menu
+	private MenuHelper mMenuHelper;
 	
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		setupData();
 		bindViews();
-		setupToolbar();
+	}
+	// 设置数据
+	public void setupData(){
+		mIdMap = new ArrayMap<String,Integer>();
+		mFragmentList = new ArrayList<Fragment>();
+		mMenuHelper = new MenuHelper();
 	}
 	// 绑定控件
 	public void bindViews(){
 		toolbar = (Toolbar)findViewById(R.id.main_toolbar);
 		mDrawerLayout = (DrawerLayout)findViewById(R.id.main_drawerlayout);
 		mNavView = (NavigationView)findViewById(R.id.main_nav_view);
+		setupToolbar();
 	}
 	// 设置Toolbar和抽屉
 	public void setupToolbar(){
@@ -64,8 +83,35 @@ public class MainActivity extends AppCompatActivity{
 			}
 		});
 	}
-	// 添加Item
-	public void addNavItem(Integer id,String name){
-		// 先留空，以后再弄
+	private class MenuHelper{
+		// id
+		int menu_id = 1;
+		// Fragment
+		FragmentManager manager;
+		FragmentTransaction transaction;
+		
+		// 构造
+		MenuHelper(){
+			manager = getSupportFragmentManager();
+			transaction = manager.beginTransaction();
+		}
+		// 添加Fragment
+		void addFragment(File file){
+			// 留空，什么时候能够使用编辑功能就加上
+		}
+		// 添加Item
+		void addNavItem(String id,String name,Fragment fragment,boolean needToOpen){
+			// 数据
+			mIdMap.put(id,menu_id);
+			mFragmentList.add(fragment);
+			// Menu
+			Menu menu = mNavView.getMenu();
+			menu.add(0,menu_id,0,name).setIcon(R.drawable.ic_edit_black);
+			// 启动Fragment
+			if(needToOpen){
+				transaction.replace(R.id.main_container,fragment);
+				transaction.commit();
+			}
+		}
 	}
 }
